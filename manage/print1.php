fb630f55cd31453f48836eacdc2331fe6ece15b4
amin <amin4udin@gmail.com>
2018-02-05 13:25:31 +0700

    v1.0

--- manage/print1.php
+++ manage/print1.php
@@ -6,37 +6,63 @@ if (empty($_SESSION['username'])) {
   header("Location: ../index.php");
 }
 ?>
+<html>
+<head>
+  <title></title>
+</head>
+<style type="text/css">
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+<body>
+<b>Data Semua Pegawai.</b>
 <table>
-  <thead>
-  <tr>
-  <th>No</th>
-  <th>Nama</th>
-  <th>Jabatan</th>
-  <th>alamat</th>
-  <th>Telp</th>
-  <th>Kelamin</th>
-   <th><a href="editpeg.php?tambah"><font class="glyphicon glyphicon-plus"></font></a></th>
-  </tr>
+    <thead>
+      <tr>
+      <th>No</th>
+      <th>Nama</th>
+      <th>Jabatan</th>
+      <th>alamat</th>
+      <th>Telp</th>
+      <th>Kelamin</th>
+       <th><a href="editpeg.php?tambah"><font class="glyphicon glyphicon-plus"></font></a></th>
+      </tr>
     </thead>
     <?php
       $query = mysqli_query($konek, "select * from pegawai limit 3");{
       while ($t = mysqli_fetch_array($query)) {
     ?>
-  <tbody>
-  <tr>
-  <td><?php echo $t['1']; ?></td>
-  <td><?php echo $t['2']; ?></td>
-  <td><?php echo $t['3']; ?></td>
-  <td><?php echo $t['4']; ?></td>
-  <td><?php echo $t['5']; ?></td>
-  <td><?php echo $t['6']; ?></td>
-  </tr>
-  </tbody>
+    <tbody>
+      <tr>
+      <td><?php echo $t['1']; ?></td>
+      <td><?php echo $t['2']; ?></td>
+      <td><?php echo $t['3']; ?></td>
+      <td><?php echo $t['4']; ?></td>
+      <td><?php echo $t['5']; ?></td>
+      <td><?php echo $t['6']; ?></td>
+      </tr>
+    </tbody>
   <?php
     }
   }
   ?>
 </table>
+</body>
+</html>
+
 <?php
 $html = ob_get_contents();
 ob_end_clean();
